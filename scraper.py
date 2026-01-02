17cebfb6932719b18810aa5432b3ba193913cfbb
Sirmadeira <nicolaumori@gmail.com>
2026-01-02 12:53:39 -0300

    Feat: Advancing my ranker improving the scraper

--- scraper.py
+++ scraper.py
@@ -1,20 +1,25 @@
 import polars as pl
 import logging
+import time
 import requests
 from pathlib import Path
 from datetime import datetime
 
 
-def fetch_all_reddit_json(subreddits: list[str], limit: int) -> list[dict]:
+def get_reddit_jsons(
+    subreddits: list[str], sort="hot", pages=5, limit=100, timeframe="all"
+) -> list[dict]:
     """
-    Fetch Reddit JSON listings concurrently.
+    Fetch Reddit JSON listings
 
     Args:
         subreddits: List of subreddit base URLs.
-        limit: Number of posts to fetch per subreddit.
+        limit: Number of posts to fetch per subreddit.Caps at 100
+        pages: The amount of pages to scrap. If at 100, 5 pages equal 500 posts
+        sort: The sort type for posts
 
     Returns:
-        A list of dicts that were previously jsons.
+        A list of dicts.
     """
 
     # Headers to ofuscate identification
@@ -25,15 +30,35 @@ def fetch_all_reddit_json(subreddits: list[str], limit: int) -> list[dict]:
             "Chrome/120.0.0.0 Safari/537.36"
         )
     }
-    results = [
-        requests.get(f"{url}/.json?limit={limit}", headers=headers).json()
-        for url in subreddits
-    ]
+    params = {"limit": limit, "t": timeframe}
 
-    return results
+    all_posts = []
+    after = None
+    for subreddit in subreddits:
+        for page in range(pages):
+            if after:
+                logging.debug("Grabbing new page data")
+                params["after"] = after
+
+            url = f"{subreddit}/{sort}.json"
+            response = requests.get(url, headers=headers, params=params)
+
+            if response.status_code == 200:
+                data = response.json()
+                posts = data["data"]["children"]
+                all_posts.extend(posts)
+
+                after = data["data"].get("after")
+                if not after:
+                    break
+
+                time.sleep(0.2)
+
+    logging.debug(f"Sample json:{all_posts[0]}")
+    return all_posts
 
 
-def extract_meme_data_reddit(loaded_jsons: list[dict]) -> pl.DataFrame:
+def extract_meme_data_reddit(all_posts: list[dict]) -> pl.DataFrame:
     """
     Intakes a list of dicts and extracts only the needed meme data.
 
@@ -43,40 +68,32 @@ def extract_meme_data_reddit(loaded_jsons: list[dict]) -> pl.DataFrame:
     Returns:
         Polars DataFrame containing normalized meme metadata.
     """
-    posts: list[dict] = []
+    infos: list[dict] = []
 
-    for loaded_json in loaded_jsons:
-        children = loaded_json.get("data", {}).get("children", [])
-        if not children:
-            logger.warning("Empty Reddit listing encountered")
-            continue
-
-        for child in children:
-            data = child["data"]
-            posts.append(
-                {
-                    "created_utc": data.get("created_utc"),
-                    "title": data.get("title"),
-                    "score": data.get("score"),
-                    "upvotes": data.get("ups"),
-                    "num_comments": data.get("num_comments"),
-                    "upvote_ratio": data.get("upvote_ratio"),
-                    "is_created_from_ads_ui": data.get("is_created_from_ads_ui"),
-                    "total_awards_received": data.get("total_awards_received"),
-                    "num_reports": data.get("num_reports"),
-                    "image_url": data.get("url_overridden_by_dest"),
-                    "is_video": data.get("is_video"),
-                    "sub_reddit": data.get("subreddit")
-                }
-            )
+    for post in all_posts:
+        data = post["data"]
+        infos.append(
+            {
+                "created_utc": data.get("created_utc"),
+                "title": data.get("title"),
+                "upvotes": data.get("ups"),
+                "num_comments": data.get("num_comments"),
+                "upvote_ratio": data.get("upvote_ratio"),
+                "is_created_from_ads_ui": data.get("is_created_from_ads_ui"),
+                "total_awards_received": data.get("total_awards_received"),
+                "num_reports": data.get("num_reports"),
+                "image_url": data.get("url_overridden_by_dest"),
+                "is_video": data.get("is_video"),
+                "sub_reddit": data.get("subreddit"),
+            }
+        )
 
     df = (
-        pl.DataFrame(posts)
+        pl.DataFrame(infos)
         .with_columns(pl.from_epoch("created_utc", time_unit="s").alias("created_at"))
         .with_columns(
             [
                 pl.col("created_utc").cast(pl.Int64),
-                pl.col("score").cast(pl.Int64),
                 pl.col("upvotes").cast(pl.Int64),
                 pl.col("num_comments").cast(pl.Int64),
                 pl.col("upvote_ratio").cast(pl.Float64),
@@ -104,17 +121,19 @@ def setup_logging(time: str):
 
 
 if __name__ == "__main__":
+    # Logging setup
     now = datetime.now()
     formatted = now.strftime("%Y_%m_%d%_H:%M:%S.%f")[:-3]
     setup_logging(time=formatted)
+
     SUBREDDITS = [
         "https://www.reddit.com/r/dankmemes/",
         "https://www.reddit.com/r/memes/",
         "https://www.reddit.com/r/Memes_Of_The_Dank/",
     ]
     # Extract
-    loaded_jsons = fetch_all_reddit_json(SUBREDDITS, limit=200)
-    # Slight Transform
-    df = extract_meme_data_reddit(loaded_jsons)
+    all_posts = get_reddit_jsons(SUBREDDITS, pages=5)
+    # Transform to dataframe
+    df = extract_meme_data_reddit(all_posts)
     # Dump
     df.write_parquet(f"data/reddit_data_{formatted}.parquet")
